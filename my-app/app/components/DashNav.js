2673869d35abaa615a9fee820a5248ec5c1bae7e
Michael Chuang <85854593+mchuang413@users.noreply.github.com>
2024-12-19 20:47:51 -0800

    Update DashNav.js

--- my-app/app/components/DashNav.js
+++ my-app/app/components/DashNav.js
@@ -37,6 +37,68 @@ const FlyoutNav = () => {
   );
 };
 
+const MobileMenuLink = ({ children, href, FoldContent, setMenuOpen }) => {
+  const [ref, { height }] = useMeasure();
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="relative text-neutral-950">
+      {FoldContent ? (
+        <div
+          className="flex w-full cursor-pointer items-center justify-between border-b border-neutral-300 py-6 text-start text-2xl font-semibold"
+          onClick={() => setOpen((pv) => !pv)}
+        >
+          <a
+            onClick={(e) => {
+              e.stopPropagation();
+              setMenuOpen(false);
+            }}
+            href={href}
+          >
+            {children}
+          </a>
+          <motion.div
+            animate={{ rotate: open ? "180deg" : "0deg" }}
+            transition={{
+              duration: 0.3,
+              ease: "easeOut",
+            }}
+          >
+            <FiChevronDown />
+          </motion.div>
+        </div>
+      ) : (
+        <a
+          onClick={(e) => {
+            e.stopPropagation();
+            setMenuOpen(false);
+          }}
+          href="#"
+          className="flex w-full cursor-pointer items-center justify-between border-b border-neutral-300 py-6 text-start text-2xl font-semibold"
+        >
+          <span>{children}</span>
+          <FiArrowRight />
+        </a>
+      )}
+      {FoldContent && (
+        <motion.div
+          initial={false}
+          animate={{
+            height: open ? height : "0px",
+            marginBottom: open ? "24px" : "0px",
+            marginTop: open ? "12px" : "0px",
+          }}
+          className="overflow-hidden"
+        >
+          <div ref={ref}>
+            <FoldContent />
+          </div>
+        </motion.div>
+      )}
+    </div>
+  );
+};
+
 const Logo = ({ color = "white" }) => (
   <div className="flex items-center gap-2">
     <span className="text-2xl font-bold" style={{ color }}>
