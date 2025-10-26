6872a25ebeb56166d54951cdef477037ab82b37c
Michael Chuang <lxsquidofficial@gmail.com>
2025-10-25 21:05:01 -0700

    Update Navbar.js

--- my-app/app/components/Navbar.js
+++ my-app/app/components/Navbar.js
@@ -79,7 +79,6 @@ const FlyoutNav = () => {
       <div className="mx-auto flex max-w-7xl items-center justify-between">
         <Logo />
         <div className="hidden gap-6 lg:flex">
-          <Links />
           <CTAs />
         </div>
         <MobileMenu />
@@ -115,57 +114,6 @@ const Logo = ({ color = "white" }) => {
   );
 };
 
-const Links = () => {
-  return (
-    <div className="flex items-center gap-6">
-      {LINKS.map((l) => (
-        <NavLink key={l.text} href={l.href} FlyoutContent={l.component}>
-          {l.text}
-        </NavLink>
-      ))}
-    </div>
-  );
-};
-
-const NavLink = ({ children, href, FlyoutContent }) => {
-  const [open, setOpen] = useState(false);
-
-  const showFlyout = FlyoutContent && open;
-
-  return (
-    <div
-      onMouseEnter={() => setOpen(true)}
-      onMouseLeave={() => setOpen(false)}
-      className="relative h-fit w-fit"
-    >
-      <a href={href} className="relative">
-        {children}
-        <span
-          style={{
-            transform: showFlyout ? "scaleX(1)" : "scaleX(0)",
-          }}
-          className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-indigo-300 transition-transform duration-300 ease-out"
-        />
-      </a>
-      <AnimatePresence>
-        {showFlyout && (
-          <motion.div
-            initial={{ opacity: 0, y: 15 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 15 }}
-            style={{ translateX: "-50%" }}
-            transition={{ duration: 0.3, ease: "easeOut" }}
-            className="absolute left-1/2 top-12 bg-white text-black"
-          >
-            <div className="absolute -top-6 left-0 right-0 h-6 bg-transparent" />
-            <div className="absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white" />
-            <FlyoutContent />
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
-  );
-};
 
 const CTAs = () => {
   return (
@@ -248,48 +196,5 @@ const MobileMenuLink = ({ children, href, FoldContent, setMenuOpen }) => {
   );
 };
 
-const MobileMenu = () => {
-  const [open, setOpen] = useState(false);
-  return (
-    <div className="block lg:hidden">
-      <button onClick={() => setOpen(true)} className="block text-3xl">
-        <FiMenu />
-      </button>
-      <AnimatePresence>
-        {open && (
-          <motion.nav
-            initial={{ x: "100vw" }}
-            animate={{ x: 0 }}
-            exit={{ x: "100vw" }}
-            transition={{ duration: 0.15, ease: "easeOut" }}
-            className="fixed left-0 top-0 flex h-screen w-full flex-col bg-white"
-          >
-            <div className="flex items-center justify-between p-6">
-              <Logo color="black" />
-              <button onClick={() => setOpen(false)}>
-                <FiX className="text-3xl text-neutral-950" />
-              </button>
-            </div>
-            <div className="h-screen overflow-y-scroll bg-neutral-100 p-6">
-              {LINKS.map((l) => (
-                <MobileMenuLink
-                  key={l.text}
-                  href={l.href}
-                  FoldContent={l.component}
-                  setMenuOpen={setOpen}
-                >
-                  {l.text}
-                </MobileMenuLink>
-              ))}
-            </div>
-            <div className="flex justify-end bg-neutral-950 p-6">
-              <CTAs />
-            </div>
-          </motion.nav>
-        )}
-      </AnimatePresence>
-    </div>
-  );
-};
 
 export default Example;
